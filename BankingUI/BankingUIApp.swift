93b2d0f598d20166b1d38ee97bf7e0f1eedbe40f
Sagar Patel <s.72427patel@gmail.com>
2020-07-23 01:01:10 -0400

    Initial Commit

---
+++ BankingUI/BankingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingUIApp.swift
+//  BankingUI
+//
+//  Created by Sagar Patel on 2020-07-23.
+//
+
+import SwiftUI
+
+@main
+struct BankingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankingUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BankingUI
+//
+//  Created by Sagar Patel on 2020-07-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
